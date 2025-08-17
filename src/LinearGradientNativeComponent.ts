465a58374bdceb808ba60041ad14ccac1a515d9b
wangzhi <wangzhi0114@163.com>
2025-08-17 20:47:19 +0800

    整理逻辑

--- src/LinearGradientNativeComponent.ts
+++ src/LinearGradientNativeComponent.ts
@@ -1,14 +1,18 @@
-import type { ViewProps } from "ViewPropTypes";
-import type { HostComponent } from "react-native";
-import codegenNativeComponent from "react-native/Libraries/Utilities/codegenNativeComponent";
+import type {HostComponent, ViewProps} from 'react-native';
+import codegenNativeComponent from 'react-native/Libraries/Utilities/codegenNativeComponent';
 
 import type { ColorValue } from "react-native/Libraries/StyleSheet/StyleSheet";
 import type { Float } from "react-native/Libraries/Types/CodegenTypes";
+import type {PointValue} from 'react-native/Libraries/StyleSheet/StyleSheetTypes';
 
-
-type PointValue = {
-  x: Float
-  y: Float
+export interface LinearGradientProps extends ViewProps {
+  colors: ColorValue[];
+  start?: PointValue;
+  end?: PointValue;
+  locations?: Float[];
+  useAngle?: boolean;
+  angleCenter?: PointValue;
+  angle?: Float;
 }
 
 export interface Props extends ViewProps {
---
+++ src/index.ts
@@ -0,0 +1,6 @@
+
+import LinearGradient from './LinearGradientNativeComponent';
+
+export default LinearGradient;
+export {default as LinearGradient} from './LinearGradientNativeComponent';
+export * from './LinearGradientNativeComponent';
